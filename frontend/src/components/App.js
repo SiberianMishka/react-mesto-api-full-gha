20305dce68490f06c4a1366dce15f371b57b3726
Ivan Stremaus <i.stremaus@yandex.ru>
2023-07-14 08:55:41 +0600

    PR15: fix likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -71,7 +71,7 @@ export default function App() {
 
   function handleCardLike(card) {
     // Проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some(id => id === currentUser._id);
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
